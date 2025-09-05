9faa839a3df920d44ba4f167058283c1860d23b1
LeonardoRSantos <leonardoworkbench@gmail.com>
2025-09-04 22:09:50 -0300

    Ajustes e criacão de component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PaginaPrincipalComponent } from './pages/public/pagina-principal/pagina-principal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PaginaPrincipalComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('health-test-front-end');
 }
+
